f04167550f57eefed95a8d1985a27bc428b5737f
AlexNuzhdov <alexnuzhdov90@gmail.com>
2022-11-13 23:45:38 +0200

    fix path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,7 +38,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>   
         
-        <Route path="*" element={<div>Not found</div>} />
+        <Route path="*" element={<Home />} />
         
 
       </Routes> 
